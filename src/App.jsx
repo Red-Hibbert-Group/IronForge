b508082ce9481cf14fce2b4e268d12a934f6ac9f
Meghana661 <anirudhvanapali221@gmail.com>
2025-07-31 23:48:32 +0530

    Scroll issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { ThemeProvider, CssBaseline } from '@mui/material';
+import { useEffect } from 'react';
 import Home from './pages/Home';
 import Services from './pages/Services';
 import About from './pages/About';
@@ -9,11 +10,23 @@ import Resources from './pages/Resources';
 import Blog from './pages/Blog';
 import { theme } from './theme/theme';
 
+// ScrollToTop component to handle scroll restoration
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/services" element={<Services />} />
